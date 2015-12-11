0e24e8605fc75c0c48c6d4d3de1255637687d088
17lreverman <17lreverman@students.madison.k12.in.us>
2015-12-11 13:43:47 -0500

    added player 4

--- Ben Ratcliff/PrisonersGame_CrimeEdition.py
+++ Ben Ratcliff/PrisonersGame_CrimeEdition.py
@@ -215,7 +215,15 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     # YOU MAY CHANGE THE FOLLOWING CODE:
 
     elif player == 4:
-        return 'Change this Agent';
+        if getting_team_name:
+            return 'Elf'
+        else:
+            if len(opponent_history)==0:
+                return 'c'
+            elif history[-1]=='c' and opponent_history[-1]=='b':
+                return 'b'
+            else:
+                return 'c' 
 
 
 
