0884abdb4eb3fe429eaf5395c20c65b1025d3239
17bratcliff <17bratcliff@students.madison.k12.in.us>
2015-12-11 14:08:35 -0500

    Update PrisonersGame_CrimeEdition.py

--- Ben Ratcliff/PrisonersGame_CrimeEdition.py
+++ Ben Ratcliff/PrisonersGame_CrimeEdition.py
@@ -296,7 +296,7 @@ elif player == 7:
     ######
     ######
     #
-    elif wraithEnabled == True and player == 5:     #Hint: WRAITHS ALWATYS WIN!!! MUHAHAHAHAHA!
+    elif wraithEnabled == True and player == 8:     #Hint: WRAITHS ALWATYS WIN!!! MUHAHAHAHAHA!
         if getting_team_name:
             return 'Wraith'
         else:
